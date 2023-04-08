1d80a300a37ddfb66d14e9e144daee61e5f9ab2c
Felipe Sales <soufeliposales@gmail.com>
2023-04-08 09:58:07 -0300

    initial commit

---
+++ bin/dart_api_v2.dart
@@ -0,0 +1,5 @@
+import 'package:dart_api_v2/dart_api_v2.dart' as dart_api_v2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_api_v2.calculate()}!');
+}
---
+++ test/dart_api_v2_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_api_v2/dart_api_v2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
