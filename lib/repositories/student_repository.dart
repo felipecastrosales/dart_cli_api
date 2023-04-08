c2167fd949dc72d5218e87650d0accd7c5a093d8
Felipe Sales <soufeliposales@gmail.com>
2023-04-08 14:55:23 -0300

    feat: created repository

---
+++ lib/repositories/student_repository.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'package:dart_api_v2/models/student.dart';
+
+class StudentsRepositories {
+  Future<List<Student>> getStudents() async {
+    final studentsResponse = await http.get(
+      Uri.parse('http://localhost:7777/students'),
+    );
+    final studentsList = jsonDecode(studentsResponse.body);
+    return studentsList
+        .map<Student>((student) => Student.fromMap(student))
+        .toList();
+  }
+
+  Future<Student> getById(int id) async {
+    final studentResponse = await http.get(
+      Uri.parse('http://localhost:7777/students/$id'),
+    );
+    return Student.fromJson(studentResponse.body);
+  }
+}
